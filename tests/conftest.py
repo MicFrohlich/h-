e83e073b7622a6f98427c0b54f076fb794813545
Michael <mike.frohl@gmail.com>
2022-06-06 21:43:32 +0200

    feat: add basic test implementation for basic flask service

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
-from ..app import app
-
+from app import app
 
 @pytest.fixture()
-def client(app):
+def client():
+    """Return a test client"""
     return app.test_client()
 
 
 @pytest.fixture()
-def runner(app):
+def runner():
+    """Return a runner"""
     return app.test_cli_runner()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,26 @@
+import json
+
 def test_request_example(client):
+    """Test basic root route"""
     response = client.get("/")
-    assert b"<h2>Hello, World!</h2>" in response.data
+    assert b"Try /appointments or /profiles" in response.data
+
+def test_appointments(client):
+    """Test appointments route"""
+    f = open("mock_data.json")
+    data = json.load(f)
+    appointments = []
+    for i in data["appointments"]:
+        appointments.append(data["appointments"][i])
+    response = client.get("/appointments")
+    assert b"profile_requestor_pk" in response.data
+    
+def test_profiles(client):
+    """Test profiles route"""
+    f = open("mock_data.json")
+    data = json.load(f)
+    profiles = []
+    for i in data["profiles"]:
+        profiles.append(data["profiles"][i])
+    response = client.get("/profiles")
+    assert b"1" in response.data
